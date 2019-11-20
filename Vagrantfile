b7e128322b209a8c994a171d71503f3ca130ec39
Nathan Corvino <nathan@corvino.com>
2019-11-19 23:41:05 -0800

    Vagrant config for Prometheus, Grafana, Swift, and Vapor.

--- Vagrantfile
+++ Vagrantfile
@@ -4,4 +4,14 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/bionic64"
   config.vm.synced_folder ".", "/vagrant", disabled: true
+  config.vm.provision "shell", path: "provision.sh"
+
+  # Vapor
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  # Grafana
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  # Prometheus
+  config.vm.network "forwarded_port", guest: 9090, host: 9090
+  # node_exporter
+  config.vm.network "forwarded_port", guest: 9100, host: 9100
 end
--- provision.sh
+++ provision.sh
@@ -1,11 +1,18 @@
 #! /usr/bin/env bash
 
-echo "apt update"
-apt update > /dev/null 2&>1
-
 # https://docs.vapor.codes/3.0/install/ubuntu/
 echo "add vapor repo"
 eval "$(curl -sL https://apt.vapor.sh)"
 
+# https://grafana.com/docs/installation/debian/
+add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+echo "apt update"
+apt update # > /dev/null 2&>1
+
 echo "install packages"
 apt install -y supervisor prometheus grafana vapor
+
+systemctl enable grafana-server.service
+systemctl start grafana-server
